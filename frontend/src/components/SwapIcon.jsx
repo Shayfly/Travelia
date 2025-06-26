9c6e49554d06bde4ebed9141727f0b178d1a8284
Shayfly <shaydoors84@gmail.com>
2025-06-26 21:01:07 +0300

    feat: add trip type and swap to hero search

---
+++ frontend/src/components/SwapIcon.jsx
@@ -0,0 +1,12 @@
+export default function SwapIcon({ className = "w-5 h-5" }) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      fill="currentColor"
+      className={className}
+    >
+      <path d="M4 7h13l-3.5-3.5L15 2l6 6-6 6-1.5-1.5L17 9H4V7zm16 10H7l3.5 3.5L9 22l-6-6 6-6 1.5 1.5L7 15h13v2z" />
+    </svg>
+  );
+}
