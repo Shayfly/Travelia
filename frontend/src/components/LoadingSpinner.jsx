f4f56092adbc93ff343e90b8ce986284985524aa
Shayfly <shaydoors84@gmail.com>
2025-06-26 12:01:39 +0300

    Improve hotel search error handling

---
+++ frontend/src/components/LoadingSpinner.jsx
@@ -0,0 +1,3 @@
+export default function LoadingSpinner({ className = 'w-6 h-6 border-4 border-blue-500 border-t-transparent rounded-full animate-spin' }) {
+  return <div className={className}></div>;
+}
